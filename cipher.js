d1a6160a2d147cae5a4701624b6e77a6ddd9423f
MoSi162 <mohamed.sidali@ymail.com>
2025-09-26 14:41:17 +0200

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -21,26 +21,3 @@ function encryptChar(char, shiftAmount) {
     let shiftedPos = (code - 97 + shiftAmount) % 26; // Shift within 0-25 positions
 
     // Handle negative shifts, JavaScript's % can be negative
-    if (shiftedPos < 0) {
-      shiftedPos += 26;
-    }
-
-    // Get the encrypted character code and convert back to letter
-    return String.fromCharCode(97 + shiftedPos);
-  } else {
-    // If not a letter, return the character as is (for spaces, punctuation)
-    return char;
-  }
-}
-
-// Go through each character in the input phrase and encrypt it
-let encrypted = "";
-
-for (let i = 0; i < input.length; i++) {
-  // Encrypt each character and add it to the result string
-  encrypted += encryptChar(input[i], shift);
-}
-
-// Print the encrypted phrase
-console.log(encrypted);
-
